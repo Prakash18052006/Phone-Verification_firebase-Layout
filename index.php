ad99129d9ecd8d5cf07c0c29f1832d3a7fa2daa6
Prakash Choudhary <162735839+Prakash18052006@users.noreply.github.com>
2024-10-12 15:20:29 +0530

    ADD

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Firebase Phone Verification</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <form action="verification.php">
+      <h1>Firebase Phone verification In PHP</h1>
+      <div class="formcontainer">
+      <hr/>
+      <div class="container">
+        <label for="uname"><strong>Phone Number</strong></label>
+        <input type="text" id="number" placeholder="Enter phone number" name="uname" required>
+      </div>
+      <div id="recaptcha-container"></div>
+      <button type="button" onclick="phoneAuth();">Send Otp</button>
+    
+    </form>
+
+
+    <script src="https://www.gstatic.com/firebasejs/8.3.1/firebase.app.js"></script>
+    <script type="module">
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
+  
+    const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    </script>
+    <script src="firebase.js" type="text/javascript"></script>
+  </body>
+</html>
